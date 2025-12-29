e4f60a13460089b39d68329bcb74d615c7614fc3
Tammineedi Hemanth <tammhemanth2006@gmail.com>
2025-12-29 09:08:04 +0530

    Algorithm for printing Nodes at distance K from root.

---
+++ Trees/Node_at_distance.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left = nullptr;
+    Node* right = nullptr;
+
+    Node(int data=0)
+    {
+        this->data = data;
+    }
+};
+
+Node* constructBinaryTree(const vector<int>& arr)
+{
+    const int len = arr.size();
+
+    if(arr.empty() || len>1024)
+        return nullptr;
+
+    Node* root = new Node(arr[0]);
+    queue<Node*> q;
+    q.push(root);
+
+    int i=1;
+
+    while(i<len)
+    {
+        Node* current = q.front();
+        q.pop();
+
+        if(i<len)
+        {
+            current->left = new Node(arr[i++]);
+            q.push(current->left);
+        }
+
+        if(i<len)
+        {
+            current->right = new Node(arr[i++]);
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
+
+void printNodesAtDistance(Node* root, int K)
+{
+    if(root==nullptr || K<0)
+        return;
+
+    if(K==0)
+    {
+        cout<<root->data<<',';
+        return;
+    }
+
+    printNodesAtDistance(root->left, K-1);
+    printNodesAtDistance(root->right, K-1);
+}
+
+int main()
+{
+    vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+
+    Node* root = constructBinaryTree(v);
+    printNodesAtDistance(root, 2);
+}
